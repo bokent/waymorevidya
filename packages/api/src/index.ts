6ada6b71ed19f57c6b677cb4c09b478541511b9a
dmshvetsov <shvetsovdm@gmail.com>
2023-02-20 13:00:30 +1100

    add api package

---
+++ packages/api/src/index.ts
@@ -0,0 +1,13 @@
+import * as express from 'express'
+import { Request, Response } from 'express'
+
+const app = express()
+const PORT = process.env.PORT || 8080
+
+app.get('/health', (req: Request, res: Response) => {
+  res.json({ status: 'healthy' })
+})
+
+app.listen(PORT, () => {
+  console.log(`waymorevidya is up in running on port ${PORT}`)
+})
