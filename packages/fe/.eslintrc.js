994d16ecdeeb6d14837fb761f309464cc8d2b713
dmshvetsov <shvetsovdm@gmail.com>
2023-02-20 15:25:03 +1100

    add frontend packages with craco and mantine UI

---
+++ packages/fe/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'standard',
+    'react-app',
+    'react-app/jest',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {}
+}
