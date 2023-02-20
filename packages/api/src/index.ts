98e5d632f43bd004b2eec1056ec9d394330fcfea
dmshvetsov <shvetsovdm@gmail.com>
2023-02-20 14:53:31 +1100

    add prettier, eslint to api package

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,7 +4,7 @@ import { Request, Response } from 'express'
 const app = express()
 const PORT = process.env.PORT || 8080
 
-app.get('/health', (req: Request, res: Response) => {
+app.get('/health', (_req: Request, res: Response) => {
   res.json({ status: 'healthy' })
 })
 
